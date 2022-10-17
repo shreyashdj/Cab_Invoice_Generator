47a9b843a7beb58d2e5dafb1989ba26247c40e92
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-18 00:03:17 +0530

    [Shreyash]: Added code to store ride data

--- Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/Ride.java
+++ Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/Ride.java
@@ -4,10 +4,17 @@ package com.bridgelabz.cabInvoiceGenerator;
  * @author - Shreyash Jadhav
  */
 public class Ride {
+    /*
+     *  variables to store ride data
+     */
+    public String rideType;
     public double distance;
     public double time;
-
-    public Ride(double distance, double time) {
+    /**
+     *  created constructor for Ride class
+     */
+    public Ride(String rideType, double distance, double time) {
+        this.rideType = rideType;
         this.distance = distance;
         this.time = time;
     }
