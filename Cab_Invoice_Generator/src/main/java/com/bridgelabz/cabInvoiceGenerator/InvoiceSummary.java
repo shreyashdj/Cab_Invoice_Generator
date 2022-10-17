7f3ca1bcdf6969df339be41053afedf465c442e8
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-18 00:04:26 +0530

    [Shreyash]: Added code to generate invoice summary of user

--- Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/InvoiceSummary.java
+++ Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/InvoiceSummary.java
@@ -6,15 +6,23 @@ import java.util.Objects;
  * @author - Shreyash Jadhav
  */
 public class InvoiceSummary {
+    /*
+     *  variable to store data of Invoice Summary
+     */
     public final int numberOfRides;
     public final double totalFare;
     public final double averageFare;
-
+    /**
+     *  created constructor of class InvoiceSummary
+     */
     public InvoiceSummary(int numberOfRides, double totalFare) {
         this.numberOfRides = numberOfRides;
         this.totalFare = totalFare;
         this.averageFare = this.totalFare / this.numberOfRides;
     }
+    /**
+     *  Override equals method to compare objects
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -24,7 +32,9 @@ public class InvoiceSummary {
                 Double.compare(that.totalFare, totalFare) == 0 &&
                 Double.compare(that.averageFare, averageFare) == 0;
     }
-
+    /**
+     *  Override toString method to get string representation of the object
+     */
     @Override
     public String toString() {
         return "InvoiceSummary{" +
