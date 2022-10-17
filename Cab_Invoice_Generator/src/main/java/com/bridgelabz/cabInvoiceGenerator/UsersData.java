ca4280fcdd07413bcc02ade5037a3dd481f44b69
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-18 00:02:17 +0530

    [Shreyash]: Added code to store multiple ride data of multiple users

--- Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/UsersData.java
+++ Cab_Invoice_Generator/src/main/java/com/bridgelabz/cabInvoiceGenerator/UsersData.java
@@ -2,29 +2,46 @@ package com.bridgelabz.cabInvoiceGenerator;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * @author - Shreyash Jadhav
  */
 public class UsersData {
-    public static HashMap<String , List<Ride>> rideLists = new HashMap<>();
-
+    /*
+     *  created object of HashMap to store multiple ride data for multiple users
+     */
+    public static HashMap<String , ArrayList<Ride>> rideLists = new HashMap<>();
+    /**
+     *  created getData method to store multiple ride data for "user1" and "user2"
+     */
     public static void getData() {
+        /*
+         *  created user1Rides object of ArrayList to store multiple ride data of "user1"
+         *  created four objects of class Ride and rideType, distance, time
+         *  used add method to add all Ride object to user1Rides ArrayList
+         *  then adding that ArrayList to HashMap for key userId = "user1"
+         */
 
-        ArrayList<Ride> user1Rides = new ArrayList<>();
+         ArrayList<Ride> user1Rides = new ArrayList<>();
 
-        Ride ride1 = new Ride("Premium", 0.5, 5);
-        Ride ride2 = new Ride("Normal", 4, 8);
-        Ride ride3 = new Ride("Premium", 8, 16);
-        Ride ride4 = new Ride("Normal", 0.2, 2);
+         Ride ride1 = new Ride("Premium", 0.5, 5);
+         Ride ride2 = new Ride("Normal", 4, 8);
+         Ride ride3 = new Ride("Premium", 8, 16);
+         Ride ride4 = new Ride("Normal", 0.2, 2);
 
-        user1Rides.add(ride1);
-        user1Rides.add(ride2);
-        user1Rides.add(ride3);
-        user1Rides.add(ride4);
+         user1Rides.add(ride1);
+         user1Rides.add(ride2);
+         user1Rides.add(ride3);
+         user1Rides.add(ride4);
 
-        rideLists.put("user1", user1Rides);
+         rideLists.put("user1", user1Rides);
+
+        /*
+         *  created user2Rides object of ArrayList to store multiple ride data of "user2"
+         *  created four objects of class Ride and rideType, distance, time
+         *  used add method to add all Ride object to user2Rides ArrayList
+         *  then adding that ArrayList to HashMap for key userId = "user2"
+         */
 
         ArrayList<Ride> user2Rides = new ArrayList<>();
 
